89ad77e8fc38baf9b9553d1b567f2e295c27a7c5
sks <ssurovneva@mail.ru>
2023-04-19 00:33:40 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace slide_show
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_start = new System.Windows.Forms.Button();
+            this.button_open = new System.Windows.Forms.Button();
+            this.button_next = new System.Windows.Forms.Button();
+            this.button_previous = new System.Windows.Forms.Button();
+            this.button_stop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(851, 376);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(12, 416);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(86, 53);
+            this.button_start.TabIndex = 1;
+            this.button_start.Text = "start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // button_open
+            // 
+            this.button_open.Location = new System.Drawing.Point(490, 416);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(86, 53);
+            this.button_open.TabIndex = 2;
+            this.button_open.Text = "open";
+            this.button_open.UseVisualStyleBackColor = true;
+            this.button_open.Click += new System.EventHandler(this.button_open_Click);
+            // 
+            // button_next
+            // 
+            this.button_next.Location = new System.Drawing.Point(371, 416);
+            this.button_next.Name = "button_next";
+            this.button_next.Size = new System.Drawing.Size(86, 53);
+            this.button_next.TabIndex = 3;
+            this.button_next.Text = "next";
+            this.button_next.UseVisualStyleBackColor = true;
+            this.button_next.Click += new System.EventHandler(this.button_next_Click);
+            // 
+            // button_previous
+            // 
+            this.button_previous.Location = new System.Drawing.Point(246, 416);
+            this.button_previous.Name = "button_previous";
+            this.button_previous.Size = new System.Drawing.Size(86, 53);
+            this.button_previous.TabIndex = 4;
+            this.button_previous.Text = "previous";
+            this.button_previous.UseVisualStyleBackColor = true;
+            this.button_previous.Click += new System.EventHandler(this.button_previous_Click);
+            // 
+            // button_stop
+            // 
+            this.button_stop.Location = new System.Drawing.Point(125, 416);
+            this.button_stop.Name = "button_stop";
+            this.button_stop.Size = new System.Drawing.Size(86, 53);
+            this.button_stop.TabIndex = 5;
+            this.button_stop.Text = "stop";
+            this.button_stop.UseVisualStyleBackColor = true;
+            this.button_stop.Click += new System.EventHandler(this.button_stop_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(708, 416);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 36);
+            this.label1.TabIndex = 6;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 535);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_stop);
+            this.Controls.Add(this.button_previous);
+            this.Controls.Add(this.button_next);
+            this.Controls.Add(this.button_open);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.Button button_next;
+        private System.Windows.Forms.Button button_previous;
+        private System.Windows.Forms.Button button_stop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace slide_show
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+        List<Bitmap> images = new List<Bitmap>();
+        int np;
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = np.ToString() + "/" + images.Count().ToString();
+            timer.Interval = 500;
+            timer.Tick += nextClick;
+            timer1.Tick += changeText;
+        }
+
+        private void nextClick(object sender, EventArgs e)
+        {
+            if (images.Count == 0)
+            {
+                return;
+            }
+            np++;
+            if (np >= images.Count)
+            {
+                np = 0;
+            }
+            pictureBox1.Image = images[np];
+        }
+
+        
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            if(images.Count!= 0)
+            {
+                timer.Start();
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("Папка не выбрана");
+            }
+        }
+
+        private void button_stop_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            timer1.Stop();
+        }
+
+        private void button_previous_Click(object sender, EventArgs e)
+        {
+            if (images.Count == 0)
+            {
+                return;
+            }
+            np--;
+            if (np < 0)
+            {
+                np = images.Count - 1;
+            }
+            pictureBox1.Image = images[np];
+            label1.Text = (np + 1).ToString() + "/" + images.Count().ToString();
+        }
+
+        private void button_next_Click(object sender, EventArgs e)
+        {
+            if (images.Count == 0)
+            {
+                return;
+            }
+            np++;
+            if (np >= images.Count)
+            {
+                np = 0;
+            }
+            pictureBox1.Image = images[np];
+            label1.Text = (np+1).ToString() + "/" + images.Count().ToString();
+        }
+
+        private void button_open_Click(object sender, EventArgs e)
+        {
+            button_stop_Click(null, null);
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                timer.Stop();
+                if (images.Count != 0)
+                {
+                    foreach (var item in images)
+                    {
+                        item.Dispose();
+                    }
+                    images.Clear();
+                }
+                DirectoryInfo direct = new DirectoryInfo(folder.SelectedPath);
+                IEnumerable<FileInfo> all_file = direct.EnumerateFiles(); // 
+                foreach (var item in all_file)
+                {
+                    Bitmap pi = new Bitmap(item.FullName);
+                    Size pi_size = pictureBox1.Size;
+                    images.Add(new Bitmap(pi,pi_size));
+                }
+            }
+        }
+
+        private void changeText(object sender, EventArgs e)
+        {
+            label1.Text = (np + 1).ToString() + "/" + images.Count().ToString();
+        }
+    }
+}
